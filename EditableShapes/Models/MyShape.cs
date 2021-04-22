81dda0c6b05185c220477337ed0aa9d4d69b5880
southboundshawty <stevemine71@gmail.com>
2021-04-22 14:58:59 +0300

    add close to polygon

--- EditableShapes/Models/MyShape.cs
+++ EditableShapes/Models/MyShape.cs
@@ -1,7 +1,12 @@
 ﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
+using EditableShapes.Commands;
 
 namespace EditableShapes.Models
 {
@@ -12,14 +17,23 @@ namespace EditableShapes.Models
             ShapePoints = new TrulyObservableCollection<ShapePoint>();
 
             ShapePoints.CollectionChanged += ShapePoints_CollectionChanged;
-            
         }
 
         private void ShapePoints_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         { 
             OnPropertyChanged(nameof(Points));
+        }
 
+        private bool _isClosed;
 
+        public bool IsClosed
+        {
+            get { return _isClosed; }
+            set
+            {
+                _isClosed = value;
+                OnPropertyChanged();
+            }
         }
 
         private double _x;
@@ -72,6 +86,14 @@ namespace EditableShapes.Models
         }
 
         public PointCollection Points =>
-            new PointCollection(ShapePoints.Select(p => p.Position));
+            new (ShapePoints.Select(p => p.Position));
+
+        private ICommand closeShapeCommand;
+        public ICommand CloseShapeCommand => closeShapeCommand ??= new RelayCommand(CloseShape);
+
+        private void CloseShape(object commandParameter)
+        {
+            IsClosed = !IsClosed;
+        }
     }
 }
