5719df74d2d9b2d34d5b827c9b465b1df6e07298
southboundshawty <stevemine71@gmail.com>
2021-04-25 14:04:07 +0300

    add inner contours

--- EditableShapes/ViewModels/MainViewModel.cs
+++ EditableShapes/ViewModels/MainViewModel.cs
@@ -1,22 +1,25 @@
-﻿
+﻿using EditableShapes.Commands;
+using EditableShapes.Models;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+using Microsoft.Win32;
 
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using EditableShapes.Commands;
-using EditableShapes.Models;
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
-using Emgu.CV.Util;
-using Microsoft.Win32;
+
 using Brushes = System.Windows.Media.Brushes;
 using Point = System.Windows.Point;
 
@@ -31,15 +34,20 @@ namespace EditableShapes.ViewModels
 
         private ICommand browseMapCommand;
 
+        private Image<Bgr, byte> mapImage;
+
         private ICommand onMapPressedCommand;
 
         private ICommand recognizeShapesCommand;
 
-        private Image<Bgr, byte> mapImage;
-
         public MainViewModel()
         {
             MyShapes = new ObservableCollection<MyShape>();
+
+            GrayIntensity = 230;
+            KernelGauss = 5;
+
+            MagicNumber = 0.0009;
         }
 
         public ImageSource Map
@@ -48,6 +56,18 @@ namespace EditableShapes.ViewModels
             set
             {
                 _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ImageSource _grayMap;
+
+        public ImageSource GrayMap
+        {
+            get { return _grayMap; }
+            set
+            {
+                _grayMap = value;
                 OnPropertyChanged();
             }
         }
@@ -131,40 +151,131 @@ namespace EditableShapes.ViewModels
             mapImage = new Image<Bgr, byte>(path);
 
             Map = new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+
+            PreviewGrayImage();
+        }
+
+        private double _grayIntensity;
+
+        public double GrayIntensity
+        {
+            get { return _grayIntensity; }
+            set
+            {
+                _grayIntensity = value;
+                OnPropertyChanged();
+
+                PreviewGrayImage();
+            }
+        } 
+
+        private int _kernelGauss;
+
+        public int KernelGauss
+        {
+            get { return _kernelGauss; }
+            set
+            {
+                _kernelGauss = value;
+                OnPropertyChanged();
+
+                PreviewGrayImage();
+            }
+        }
+
+        private void PreviewGrayImage()
+        {
+            if (mapImage is not null)
+            {
+                Image<Gray, byte> grayImage = mapImage.SmoothGaussian(KernelGauss).Convert<Gray, byte>()
+                    .ThresholdBinaryInv(new Gray(GrayIntensity), new Gray(256));
+
+                GrayMap = ToBitmapImage(grayImage.ToBitmap());
+            }
+        }
+
+        private double _magicNumber;
+
+        public double MagicNumber
+        {
+            get { return _magicNumber; }
+            set
+            {
+                _magicNumber = value;
+                OnPropertyChanged();
+            }
         }
 
         private void RecognizeShapes(object commandParameter)
         {
-            Image<Gray, byte> grayImage = mapImage.SmoothGaussian(5).Convert<Gray, byte>()
-                .ThresholdBinaryInv(new Gray(230), new Gray(255));
+            MyShapes.Clear();
+
+            Image<Gray, byte> grayImage = mapImage.SmoothGaussian(KernelGauss).Convert<Gray, byte>()
+                .ThresholdBinaryInv(new Gray(GrayIntensity), new Gray(256));
 
             VectorOfVectorOfPoint contours = new();
 
-            Mat hierarchy = new Mat();
+            UMat hierarchy = grayImage.ToUMat();
 
-            CvInvoke.FindContours(grayImage, contours, hierarchy, RetrType.External,
+            CvInvoke.FindContours(grayImage, contours, hierarchy, RetrType.Ccomp,
                 ChainApproxMethod.ChainApproxSimple);
 
-            for (int i = 0; i < contours.Size; i++)
+            int count = contours.Size;
+
+            for (int i = 0; i < count; i++)
             {
-                MyShape s = new MyShape
+                MyShape s = new()
                 {
-                    Fill = Brushes.Gray
+                    Fill = Brushes.Gray,
+                    IsClosed = true
                 };
 
-                for (int j = 0; j < contours[i].Size; j++)
+                using (VectorOfPoint contour = contours[i])
                 {
-                    var vertex = new Point(contours[i][j].X, contours[i][j].Y);
-
-                    s.ShapePoints.Add(new ShapePoint()
+                    using (VectorOfPoint approxContour = new())
                     {
-                        Fill = Brushes.Green,
-                        Position = vertex
-                    });
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * MagicNumber, true);
+                        if (CvInvoke.ContourArea(approxContour) > 10000)
+                        {
+                            for (int j = 0; j < approxContour.Size; j++)
+                            {
+                                Point vertex = new(approxContour[j].X, approxContour[j].Y);
+
+                                s.ShapePoints.Add(new ShapePoint
+                                {
+                                    Fill = Brushes.Green,
+                                    Position = vertex
+                                });
+                            }
+                        }
+                    }
                 }
+
+                GrayMap = ToBitmapImage(grayImage.ToBitmap());
 
                 MyShapes.Add(s);
             }
+        }
+
+        private BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using MemoryStream memory = new();
+
+            bitmap.Save(memory, ImageFormat.Png);
+
+            memory.Position = 0;
+
+            BitmapImage bitmapImage = new();
+
+            bitmapImage.BeginInit();
+
+            bitmapImage.StreamSource = memory;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+
+            return bitmapImage;
         }
     }
 }
